1cf2a10204156d422edc85887ca0964aa64c2292
Elijah Corwin <raspberrypi500@yahoo.com>
2025-12-27 11:11:38 -0500

    Fix syntax errors in input statements
    Fixed the syntax errors.

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import random
 print("Welcome to Elijah's random number picker!")
 print("Please enter two numbers below (like one and ten) so the code can choose a number between them:")
 
-number_one = int(input("What is the first number? ")
-number_two = int(input("What is the second number? ")
-                 
+number_one = int(input("What is the first number? "))
+number_two = int(input("What is the second number? "))
+
 result = random.randint(number_one, number_two)
 print(f"The chosen number is:", result)
 print("Copyright (c) 2025 Elijah Corwin")
